688fef5957b1273b8872c51c0bfe83c5b51be8fe
EfrainRodriguez <efrainrg92@gmail.com>
2023-09-28 03:53:03 -0500

    configuring swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RequestMethod } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const API_PREFIX = 'api';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Products API')
+    .setDescription('The products API description')
+    .setVersion('1.0')
+    .addTag('products')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.enableCors();
   app.setGlobalPrefix(API_PREFIX, {
--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -9,30 +9,42 @@ import {
   Query,
   ValidationPipe,
 } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dtos/create-product.dto';
 import { UpdateProductDto } from './dtos/update-product.dto';
 
+@ApiTags('products')
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  @ApiOperation({ summary: 'Create a new product' })
   @Post()
   async create(@Body(new ValidationPipe()) createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
+  @ApiOperation({ summary: 'Get all products' })
   @Get()
   async findAll(@Query() query: any) {
     return this.productService.findAll(query);
   }
 
+  @ApiOperation({ summary: 'Get a product by id' })
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return this.productService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Update a product by id' })
   @Put(':id')
   async update(
     @Param('id') id: string,
@@ -41,6 +53,7 @@ export class ProductController {
     return this.productService.update(id, updateProductDto);
   }
 
+  @ApiOperation({ summary: 'Delete a product by id' })
   @Delete(':id')
   async delete(@Param('id') id: string) {
     return this.productService.delete(id);
